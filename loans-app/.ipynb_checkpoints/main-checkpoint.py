408bc41dd07a2abee9cfdc0a30783be50ebc1256
MykolaPinchuk <Mykola.Pinchuk@ur.rochester.edu>
2023-04-16 17:25:52 +0000

    refactored app to use target encoder in prediction time. works.

--- loans-app/.ipynb_checkpoints/main-checkpoint.py
+++ loans-app/.ipynb_checkpoints/main-checkpoint.py
@@ -13,46 +13,7 @@ from flask import url_for
 from xgboost import XGBClassifier
 from category_encoders import MEstimateEncoder
 
-
-class CrossFoldEncoder:
-    def __init__(self, encoder, **kwargs):
-        self.encoder_ = encoder
-        self.kwargs_ = kwargs  # keyword arguments for the encoder
-        self.cv_ = KFold(n_splits=4)
-
-    # Fit an encoder on one split and transform the feature on the
-    # other. Iterating over the splits in all folds gives a complete
-    # transformation. We also now have one trained encoder on each
-    # fold.
-    def fit_transform(self, X, y, cols):
-        self.fitted_encoders_ = []
-        self.cols_ = cols
-        X_encoded = []
-        for idx_encode, idx_train in self.cv_.split(X):
-            fitted_encoder = self.encoder_(cols=cols, **self.kwargs_)
-            fitted_encoder.fit(
-                X.iloc[idx_encode, :], y.iloc[idx_encode],
-            )
-            X_encoded.append(fitted_encoder.transform(X.iloc[idx_train, :])[cols])
-            self.fitted_encoders_.append(fitted_encoder)
-        X_encoded = pd.concat(X_encoded)
-        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
-        return X_encoded
-
-    # To transform the test data, average the encodings learned from
-    # each fold.
-    def transform(self, X):
-        from functools import reduce
-
-        X_encoded_list = []
-        for fitted_encoder in self.fitted_encoders_:
-            X_encoded = fitted_encoder.transform(X)
-            X_encoded_list.append(X_encoded[self.cols_])
-        X_encoded = reduce(
-            lambda x, y: x.add(y, fill_value=0), X_encoded_list
-        ) / len(X_encoded_list)
-        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
-        return X_encoded
+from custom_transformers import CrossFoldEncoder
 
 
 app = Flask(__name__)
@@ -90,17 +51,22 @@ def predict():
 
     with open('TEncoder.pkl', 'rb') as handle:
         tencoder = joblib.load(handle)
-
-    te_features = ['C1', 'OWN', 1]
+    
+    print(f'opened pickle successfully')
+    
+    te_features = ['D1', 'RENT', 1]
     # te_features = [data['subgrade'], data['home_ownership'], 1]
     te_features = pd.DataFrame(te_features).T
     te_features.columns = ['sub_grade', 'home_ownership', 'target']
     encoded_features = tencoder.transform(te_features)
 
-    features['sub_grade_encoded'] = encoded_features.sub_grade_encoded
-    features['home_ownership_encoded'] = encoded_features.home_ownership_encoded
+    print(f'encoded features are: {encoded_features}')
+    
+    features['sub_grade_encoded'] = encoded_features.sub_grade_encoded[0]
+    features['home_ownership_encoded'] = encoded_features.home_ownership_encoded[0]
 
-
+    print(f'features are: {features}')
+    
     prediction = get_prediction(features)
     if prediction==0:
         output = 'No deafult'
